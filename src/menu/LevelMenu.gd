d4f05455344fdc0b9049f46ee86b00cca598d50f
quentin jungo <quentin.jungo@infomaniak.com>
2023-12-02 14:41:17 +0100

    feat(ui): add menu_theme (even if its ugly)

--- src/menu/LevelMenu.gd
+++ src/menu/LevelMenu.gd
@@ -1,16 +1,21 @@
-extends Node
+extends Control
 
 @export var levels: Array[PackedScene]
 @onready var buttons_container = $ButtonsVBoxContainer
 @onready var demo_button = $ButtonsVBoxContainer/DemoButton
 
-func _ready():
-	demo_button.queue_free()
 
+func _ready():
+	_create_level_buttons()
+
+
+func _create_level_buttons(): 
 	var i = 1
 	for level in levels:
-		var new_button = Button.new()
+		var new_button = demo_button.duplicate()
 		new_button.text = "level %d" % i
 		new_button.connect("pressed", func(): get_tree().change_scene_to_packed(level))
 		buttons_container.add_child(new_button)
 		i += 1
+		
+	demo_button.queue_free()

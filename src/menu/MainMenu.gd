6ef62d5599731fde902b710ec630dcaee5503e93
quentin jungo <quentin.jungo@gmail.com>
2023-12-10 19:40:12 +0100

    fix: goto scene 0 on menu and use script classe_name instead of path

--- src/menu/MainMenu.gd
+++ src/menu/MainMenu.gd
@@ -4,5 +4,5 @@ extends Node
 @onready var level_button = $ButtonsVBoxContainer/LevelButton
 
 func _ready():
-	begin_button.connect("pressed", func(): get_tree().change_scene_to_file("res://src/levels/000.tscn"))
+	begin_button.connect("pressed", func(): LevelManager.goto_level_by_index(0))
 	level_button.connect("pressed", func(): get_tree().change_scene_to_file("res://src/menu/LevelMenu.tscn"))

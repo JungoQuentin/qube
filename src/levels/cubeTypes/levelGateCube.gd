7b221f5bfa21fa3c15b62ef1a17235b0891e6b25
quentin jungo <quentin.jungo@gmail.com>
2023-12-15 19:25:22 +0100

    init

---
+++ src/levels/cubeTypes/levelGateCube.gd
@@ -0,0 +1,32 @@
+extends Cube 
+class_name LevelGateCube
+
+@export var gate_to_level_index: int
+@export var should_be_finished_level_index: int
+var _cannot_go_color
+var _can_go_color
+var gate_open:= false:
+	set(_gate_open):
+		gate_open = _gate_open
+		_update_color()
+
+
+
+func _ready():
+	super._ready()
+	_can_go_color = _initial_color
+	_cannot_go_color = Colors.darker(_initial_color, 1, 0)
+	_update_color()
+
+
+func on_touch():
+	_touched_animation_start()
+	print(gate_open)
+	if gate_open:
+		LevelManager.goto_next_level()
+
+
+func _update_color():
+	_initial_color = _can_go_color if gate_open else _cannot_go_color
+	_touched_color = Colors.darker(_initial_color)
+	_mesh_instance.get_surface_override_material(0).albedo_color = _initial_color

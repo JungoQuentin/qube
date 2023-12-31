310ce3b0ba2885bdaf73ec1e780bb2c8620b0abf
quentin jungo <quentin.jungo@gmail.com>
2023-12-31 15:04:33 +0100

    refactor: simplify move logic (no go)

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -1,7 +1,5 @@
 class_name Player extends Node3D
 
-#region DECLARATION
-
 @onready var mesh_instance: MeshInstance3D = $MeshInstance3D
 @onready var _level: Level = get_parent()
 @export var max_transparency: float = 1
@@ -12,8 +10,6 @@ var is_moving = false
 var we_are_on_this_cube_now: Cube = null
 var joystick: Joystick
 var move_logic: CubeMoveLogic
-
-#endregion
 
 
 func _ready():
@@ -42,65 +38,41 @@ func _get_action_input():
 	if not _level.camera.is_front_player():
 		await _level.camera.go_to_player()
 		return
-	var direction: Vector3
-	match input:
-		"player_bottom":
-			direction = Vector3.DOWN
-		"player_right":
-			direction = Vector3.RIGHT
-		"player_left":
-			direction = Vector3.LEFT
-		"player_top":
-			direction = Vector3.UP
 	is_moving = true
+	var direction: Vector3 = {
+		"player_bottom": Vector3.DOWN,
+		"player_right": Vector3.RIGHT,
+		"player_left": Vector3.LEFT,
+		"player_top": Vector3.UP,
+	}[input]
 	var camera_basis = _level.camera.basis
 	direction = camera_basis * direction
 	var floor_direction = camera_basis * Vector3.FORWARD
-	move_logic = CubeMoveLogic.new(self, direction, floor_direction).init_forward_roll()
-	_roll()
+	move_logic = CubeMoveLogic.new(self, direction, floor_direction)
+	if move_logic.can_roll():
+		await _roll()
+	else:
+		await _cant_roll()
+	is_moving = false
 
 
 func _roll():
-	if move_logic.is_going_to_hole or move_logic.floor_goal is LivingCube:
-		is_moving = false
-		return
 	if move_logic._is_going_to_change_face:
 		_level.camera.player_move(move_logic._direction, move_logic._floor_direction)
 	
 	## if we are going to change face, check if we also push a moving cube
-	if move_logic.floor_neighbour is MovingCube and not move_logic.floor_neighbour.in_a_hole:
-		var neighbour: MovingCube = move_logic.floor_neighbour
-		if neighbour.can_push(move_logic._floor_direction, -move_logic._direction):
-			neighbour.on_push(move_logic._floor_direction, -move_logic._direction)
-		else:
-			is_moving = false
-			return
-	
-	var neighbour: Cube = Utils.get_raycast_collider(_level, global_position, move_logic._direction)
+	if move_logic.floor_neighbour is MovingCube and not move_logic.floor_neighbour.in_a_hole and move_logic.floor_neighbour.can_push(move_logic._floor_direction, -move_logic._direction):
+		move_logic.floor_neighbour.on_push(move_logic._floor_direction, -move_logic._direction)
 	
 	## if our neighbour is a MovingCube, we try to push him
-	if neighbour is MovingCube:
-		if neighbour.can_push(move_logic._direction, move_logic._floor_direction):
-			neighbour.on_push(move_logic._direction, move_logic._floor_direction)
-		else:
-			is_moving = false
-			return
+	var neighbour: Cube = Utils.get_raycast_collider(_level, global_position, move_logic._direction)
+	if neighbour is MovingCube and neighbour.can_push(move_logic._direction, move_logic._floor_direction):
+		neighbour.on_push(move_logic._direction, move_logic._floor_direction)
 	
 	ActionSystem.player_start_move()
-	
 	_level.player_move(move_logic._direction)
 	await move_logic.roll()
 	move_logic.floor_goal.on_touch()
-	
-	
-	## roll us back if our goal is rejecting
-	if move_logic.floor_goal and move_logic.floor_goal.is_rejecting():
-		await move_logic.roll_back()
-		move_logic.remove_pivot()
-		is_moving = false
-		ActionSystem.player_end_move()
-		return
-	
 	ActionSystem.player_end_move()
 	
 	## leave old floor and set new
@@ -109,7 +81,14 @@ func _roll():
 	we_are_on_this_cube_now = move_logic.floor_goal
 	
 	move_logic.remove_pivot()
-	is_moving = false
+
+
+func _cant_roll():
+	ActionSystem.player_start_move()
+	_level.player_move(move_logic._direction)
+	await move_logic.cant_roll()
+	move_logic.remove_pivot()
+	ActionSystem.player_end_move()
 
 ## Abort the current move and return false if there was no move
 func abort_move() -> bool:

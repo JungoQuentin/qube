9a17cf0e470ec79840c08d22ffbebc7fd0bcda37
quentin jungo <quentin.jungo@gmail.com>
2024-01-01 21:13:44 +0100

    fix: tourne autour du coin

--- src/levels/env/NaturalCamera.gd
+++ src/levels/env/NaturalCamera.gd
@@ -5,7 +5,7 @@ const camera_fov = 30.
 var is_moving = false
 var last_face: Vector3
 var is_op:= false
-
+var camera_up: Vector3
 
 func _ready():
 	fov = camera_fov
@@ -17,7 +17,9 @@ func _process(delta):
 	if not _level.player:
 		return
 	var speed = 30
+	var tourne_autour_du_coin_speed = 3
 	var goal = Vector3.ZERO
+	var camera_up_goal = _level.camera.global_basis * Vector3.UP
 	if _level.camera.mode == FixedCamera.Mode.FIXED:
 		goal = _level.camera.global_position
 		print("FIXEEED")
@@ -27,4 +29,7 @@ func _process(delta):
 	global_position.y = move_toward(global_position.y, goal.y, delta * speed)
 	global_position.z = move_toward(global_position.z, goal.z, delta * speed)
 	global_position = global_position.normalized() * Level.CAMERA_DISTANCE
-	look_at(Vector3.ZERO, _level.camera.global_basis * Vector3.UP)
+	camera_up.x = move_toward(camera_up.x, camera_up_goal.x, delta * tourne_autour_du_coin_speed)
+	camera_up.y = move_toward(camera_up.y, camera_up_goal.y, delta * tourne_autour_du_coin_speed)
+	camera_up.z = move_toward(camera_up.z, camera_up_goal.z, delta * tourne_autour_du_coin_speed)
+	look_at(Vector3.ZERO, camera_up)

87ce9b6a8f7928924716b11dda9ba501e8d4c4ce
quentin jungo <quentin.jungo@gmail.com>
2024-01-02 14:53:50 +0100

    feat: side menu logic

--- src/menu/SideMenu.gd
+++ src/menu/SideMenu.gd
@@ -1,14 +1,14 @@
-extends Control
+extends CanvasLayer
 
-@onready var buttons: VBoxContainer = $Buttons
-#@onready var go_to_menu_button: Button = $Settings/GoToMenu
-#@onready var go_to_level_gate_button: Button = $Settings/GoToLevelGateButton
+@onready var main: VBoxContainer = $Main
+@onready var current_menu: VBoxContainer = $Main
 
 
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
-	buttons.get_children().map(func(button: Button): button.pressed.connect(func(): click(button.name)))
+	main.get_children().map(func(button: Button): button.pressed.connect(func(): click(button.name)))
 	hide()
+	$Settings.hide()
 	
 
 func click(button_name: String):
@@ -23,21 +23,20 @@ func click(button_name: String):
 
 
 func go_in_sub_menu(submenu_name: String):
-	print(submenu_name)
+	current_menu.hide()
+	current_menu = find_child(submenu_name, false)
+	current_menu.show()
+	current_menu.get_child(0).grab_focus()
+
 
 func toggle_settings(to:= not visible):
 	visible = to
 	get_tree().paused = visible
 	if visible:
-		buttons.get_child(0).grab_focus()
-		$Buttons/ReturneToTitle.disabled = get_tree().current_scene is Title
-		
+		main.get_child(0).grab_focus()
+		$Main/ReturneToTitle.disabled = get_tree().current_scene is Title
 
 
 func _input(_event):
 	if Input.is_action_just_pressed("settings"):
 		toggle_settings()
-	if not visible:
-		return
-	#if Input.is_action_just_pressed("ui_down"):
-		#print("down")

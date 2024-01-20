11d8323589637122344fb48bf3b450cbd8125ba7
quentin jungo <quentin.jungo@gmail.com>
2024-01-20 16:51:44 +0100

    fix: holeCube diseaper

--- src/levels/cubeTypes/holeCube.gd
+++ src/levels/cubeTypes/holeCube.gd
@@ -7,8 +7,10 @@ var filled:= false
 func fill():
 	filled = true
 	_collision_shape.disabled = true
+	_mesh_instance.get_surface_override_material(0).albedo_color = Color.TRANSPARENT
 
 
 func reset():
 	filled = false
 	_collision_shape.disabled = false
+	_mesh_instance.get_surface_override_material(0).albedo_color = _initial_color

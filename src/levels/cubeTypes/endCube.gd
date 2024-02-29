ae58ddc5e77eb16db73cff199cecd4bd49032433
JungoQuentin <quentin.jungo@gmail.com>
2024-02-29 07:25:58 +0100

    refactor: LevelGateCube extend EndCube

--- src/levels/cubeTypes/endCube.gd
+++ src/levels/cubeTypes/endCube.gd
@@ -2,15 +2,15 @@ extends Cube
 class_name EndCube
 
 
-var _cannot_win_color
-var _can_win_color
-signal player_touch(_can_win: bool)
+var _disabled_color
+var _enabled_color
+signal player_touch
 
 
 func _ready():
 	super._ready()
-	_can_win_color = _initial_color
-	_cannot_win_color = Colors.darker(_initial_color, 1, 0)
+	_enabled_color = _initial_color
+	_disabled_color = Colors.darker(_initial_color, 1, 0)
 
 
 func on_touch():
@@ -21,6 +21,6 @@ func on_touch():
 func update_color(can_win: bool):
 	if _touch_tween and _touch_tween.is_valid():
 		_touch_tween.kill()
-	_initial_color = _can_win_color if can_win else _cannot_win_color
+	_initial_color = _enabled_color if can_win else _disabled_color
 	_touched_color = Colors.darker(_initial_color)
 	_mesh_instance.get_surface_override_material(0).albedo_color = _initial_color

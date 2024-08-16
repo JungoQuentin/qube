97788d3ffbb57bea10050f3090488ab0f4c91c34
quentin jungo <quentin.jungo@gmail.com>
2024-08-16 21:46:27 +0200

    refactor: Savable private fn

--- godot/src/save_system/Savable.gd
+++ godot/src/save_system/Savable.gd
@@ -4,7 +4,7 @@ class_name Savable
 const BASE_REFC_PROPS = [&"RefCounted", &"script", &"Built-in script"]
 
 
-func properties():
+func _properties():
 	return get_property_list() \
 		.filter(func(prop): return not prop["name"] in BASE_REFC_PROPS) \
 		.filter(func(prop): return not ".gd" in prop["name"]) \
@@ -12,21 +12,21 @@ func properties():
 
 
 func save_to_config(config: ConfigFile, section_name: String):
-	for prop_name in properties():
+	for prop_name in _properties():
 		var prop = self.get(prop_name)
 		if prop is Savable:
-			prop = prop.to_bytes_dict()
+			prop = prop._to_bytes_dict()
 		config.set_value(section_name, prop_name, prop)
 
 
-func to_bytes_dict():
+func _to_bytes_dict():
 	var result = {}
-	for prop_name in properties():
+	for prop_name in _properties():
 		result[prop_name] = var_to_bytes(self.get(prop_name))
 	return result
 
 
-static func from_bytes_dict_or_default(dict: Dictionary, default: Savable):
+static func _from_bytes_dict_or_default(dict: Dictionary, default: Savable):
 	for prop_name in dict.keys():
 		default.set(prop_name, bytes_to_var(dict[prop_name]))
 	return default
@@ -37,13 +37,13 @@ static func load_from_config_or_default(config: ConfigFile, section_name: String
 		if config.has_section_key(section_name, prop_name):
 			var value = config.get_value(section_name, prop_name)
 			if default.get(prop_name) is Savable:
-				value = Savable.from_bytes_dict_or_default(value, default.get(prop_name))
+				value = Savable._from_bytes_dict_or_default(value, default.get(prop_name))
 			default.set(prop_name, value)
 	return default
 
 ## Prints all the properties values
 func _to_string():
 	var res = ""
-	for prop in properties() :
+	for prop in _properties() :
 		res += prop + ": " + str(self.get(prop)) + ", "
 	return res
